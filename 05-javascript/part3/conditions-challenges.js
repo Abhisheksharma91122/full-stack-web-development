626b454fc81b373b7e996ab6027ea4d7e3849c5e
Abhishek Sharma <abhisheksharma270820@gmail.com>
2026-01-28 23:45:07 +0530

    add solutions for condition and array method challenges

---
+++ 05-javascript/part3/conditions-challenges.js
@@ -0,0 +1,54 @@
+// checking if a number is greater than another number
+
+let num1 = 5;
+let num2 = 9;
+
+if (num1 > num2) {
+  console.log("Number 1 is bigger than number 2");
+} else {
+  // console.log("Number 2 is bigger than number 1");
+}
+
+// Checking if a string is equal to another string
+
+let username = "Aman";
+let anotheUsername = "Aman";
+
+if (username == anotheUsername) {
+//   console.log("Pick another username");
+} else {
+  console.log("Pick this username");
+}
+
+// Checking if a variable is number or not
+
+let balance = 45;
+
+if (typeof balance === "number") {
+//   console.log("The variable is number");
+} else {
+  console.log("The variable is not a number");
+}
+
+
+// Checking if a boolean variable is true or false
+
+let isTeaReady = true;
+
+if (isTeaReady) {
+    // console.log("The tea is ready");   
+} else {
+    console.log("The tea is not ready");
+}
+
+// Checking if a array is empty or not
+
+let fruits = []
+
+// console.log(fruits.length);
+
+if ((fruits.length) === 0) {
+    console.log("The array is empty");
+} else {
+    console.log("The array is not empty");
+}
