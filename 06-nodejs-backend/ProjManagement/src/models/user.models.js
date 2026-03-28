2667ee495a09b1edf6b226076aa61f9972c18656
Abhishek Sharma <abhisheksharma270820@gmail.com>
2026-03-28 17:41:04 +0530

    feat: implement User model with bcrypt password encryption and schema hooks

---
+++ 06-nodejs-backend/ProjManagement/src/models/user.models.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: "https://placehold.co/200x200",
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Passwordd is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgetPasswordToken: {
+      type: String,
+    },
+    forgetPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
